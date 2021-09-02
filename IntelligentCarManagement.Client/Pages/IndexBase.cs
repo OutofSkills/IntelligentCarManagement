a620da7ba47ce6757f24d41ed63db3bc55645ab7
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2021-09-02 13:37:36 +0300

    fixed find location and added markers on the map feature

--- IntelligentCarManagement.Client/Pages/IndexBase.cs
+++ IntelligentCarManagement.Client/Pages/IndexBase.cs
@@ -1,18 +1,30 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using IntelligentCarManagement.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using MudBlazor;
 using System.Threading.Tasks;
 
 namespace IntelligentCarManagement.Client.Pages
 {
-    public class IndexBase: ComponentBase
+    public class IndexBase : ComponentBase
     {
+        protected Ride Ride { get; set; } = new();
         [Inject]
         public IJSRuntime JSRuntime { get; set; }
 
         protected bool displayMap = false;
+
+        protected string DestinationInputIcon = "";
+        protected string PickUpInputIcon = Icons.Filled.AddLocationAlt;
+
+        protected enum taskStates { Started, Ended };
+        protected string currentState = "";
+
+        protected override void OnInitialized()
+        {
+            Ride.PickUpLocation = "";
+            Ride.Destination = "";
+        }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
@@ -22,9 +34,89 @@ namespace IntelligentCarManagement.Client.Pages
             }
         }
 
-        protected void OpenMap()
+        protected async Task HandleSubmit()
         {
-            displayMap = !displayMap;
+
+        }
+
+        protected async Task AutoCompletePickUp()
+        {
+            await JSRuntime.InvokeVoidAsync("getPickUpLocation", null);
+        }
+        protected async Task AutoCompleteDestination()
+        {
+            await JSRuntime.InvokeVoidAsync("getDestinationLocation", null);
+        }
+
+        protected void OnPickUpInputChangeAsync(string pickUpLocation)
+        {
+            Ride.PickUpLocation = pickUpLocation;
+
+            if (pickUpLocation.Length > 0)
+            {
+                PickUpInputIcon = Icons.Filled.Clear;
+            }
+            else
+            {
+                PickUpInputIcon = Icons.Filled.AddLocationAlt;
+            }
+        }
+
+        protected void OnDestinationInputChange(string destination)
+        {
+            Ride.Destination = destination;
+
+            if (destination.Length > 0)
+            {
+                DestinationInputIcon = Icons.Filled.Clear;
+            }
+            else
+            {
+                DestinationInputIcon = "";
+            }
+        }
+
+        protected async Task OpenMapAsync()
+        {
+            if (PickUpInputIcon == Icons.Filled.AddLocationAlt)
+            {
+                // Display the map only when asking to find your location 
+                displayMap = true;
+                // Get the location
+                await JSRuntime.InvokeVoidAsync("getCurrentLocation", null);
+            }
+            else
+            {
+                //Close the map if there's no location given
+                displayMap = false;
+                // Clear the input
+                ClearInput("PickUp");
+            }
+
+            if (Ride.PickUpLocation.Length > 0)
+            {
+                // Change GPS icon to clear one
+                PickUpInputIcon = Icons.Filled.Clear;
+            }
+            else
+            {
+                // Change clear icon to GPS one
+                PickUpInputIcon = Icons.Filled.AddLocationAlt;
+            }
+        }
+
+        protected void ClearInput(string id)
+        {
+            switch (id)
+            {
+                case "PickUp":
+                    Ride.PickUpLocation = "";
+                    break;
+                case "Destination":
+                    Ride.Destination = "";
+                    DestinationInputIcon = "";
+                    break;
+            }
         }
     }
 }
--- IntelligentCarManagement.Client/wwwroot/scripts/google_maps.js
+++ IntelligentCarManagement.Client/wwwroot/scripts/google_maps.js
@@ -1,9 +1,118 @@
-﻿function initialize() {
-    var latlng = new google.maps.LatLng(40.716948, -74.003563);
+﻿
+let map, infoWindow;
+// Initialize the map in the selected div using it's id and set it's initial location
+function initialize() {
+    var latlng = new google.maps.LatLng(45.716948, -74.003563);
     var options = {
         zoom: 14, center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById
+    map = new google.maps.Map(document.getElementById
         ("map"), options);
+    infoWindow = new google.maps.InfoWindow();
+}
+
+// Get the current location of the device and set it to a textbox
+function getCurrentLocation() {
+    // Try HTML5 geolocation.
+    navigator.permissions.query({ name: 'geolocation' }).then(function (result) {
+        if (result.state == 'granted') {
+            report(result.state);
+            // If the permission is granted then retrieve the location
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        };
+                        report('Lat: ' + pos.lat);
+                        report('Long: ' + pos.lng);
+
+                        addMarker(pos);
+                        map.setCenter(pos);
+                    }
+                );
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, infoWindow, map.getCenter());
+            }
+        } else if (result.state == 'prompt') {
+            report(result.state);
+        } else if (result.state == 'denied') {
+            report(result.state);
+        }
+        result.onchange = function () {
+            report(result.state);
+        }
+    });
+}
+
+// Show a window with an error message.
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Allow browser location."
+    );
+    infoWindow.open(map);
+}
+
+function report(message) {
+    console.log(message);
+}
+
+// Get the current location of the device and set it to a textbox
+function getPickUpLocation() {
+    const center = { lat: 47.3737746, lng: 28.3859587 };
+    // Create a bounding box with sides ~10km away from the center point
+    const defaultBounds = {
+        north: center.lat + 0.1,
+        south: center.lat - 0.1,
+        east: center.lng + 0.1,
+        west: center.lng - 0.1,
+    };
+    const input = document.getElementById("pickUpLocation");
+    const options = {
+        bounds: defaultBounds,
+        componentRestrictions: { country: "md" },
+        fields: ["address_components", "geometry", "icon", "name"],
+        strictBounds: false,
+        types: ["establishment"],
+    };
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    autocomplete.bindTo("bounds", map);
+}
+
+// Get the current location of the device and set it to a textbox
+function getDestinationLocation() {
+    const center = { lat: 47.3737746, lng: 28.3859587 };
+    // Create a bounding box with sides ~10km away from the center point
+    const defaultBounds = {
+        north: center.lat + 0.1,
+        south: center.lat - 0.1,
+        east: center.lng + 0.1,
+        west: center.lng - 0.1,
+    };
+    const input = document.getElementById("destination");
+    const options = {
+        bounds: defaultBounds,
+        componentRestrictions: { country: "md" },
+        fields: ["address_components", "geometry", "icon", "name"],
+        strictBounds: false,
+        types: ["establishment"],
+    };
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    autocomplete.bindTo("bounds", map);
+}
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
 }
